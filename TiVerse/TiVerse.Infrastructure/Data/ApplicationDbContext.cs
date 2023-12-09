0d57b91756e2f3b61e3ead700f04f158fc09bfa0
Tymofii Khomych <timofey.homich@gmail.com>
2023-12-09 20:26:01 +0200

    Add project files.

---
+++ TiVerse/TiVerse.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TiVerse.TiVerse.Infrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ TiVerse/TiVerse.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TiVerse.TiVerseWebUI.Models;
+
+namespace TiVerse.TiVerseWebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View("~/TiVerse.WebUI/Views/Home/Index.cshtml");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View("~/TiVerse.WebUI/Views/Home/Privacy.cshtml");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
