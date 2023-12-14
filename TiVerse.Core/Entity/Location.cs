cec6bbb6d79aba5db0b3797b55f3b7f8cee60d65
Tymofii Khomych <timofey.homich@gmail.com>
2023-12-14 15:36:41 +0200

    Location entity

---
+++ TiVerse.Core/Entity/Location.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiVerse.Core.Entity
+{
+    internal class Location
+    {
+        public string City { get; set; }
+        public string Country { get; set; }
+        public bool BusStation { get; set; }
+        public bool RailwayStation { get; set; }
+        public bool Airport { get; set; }
+    }
+}
