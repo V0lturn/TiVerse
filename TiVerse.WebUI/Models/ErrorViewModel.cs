d207859e2c342f04ebb6c8b26f38431d0d61a997
Tymofii Khomych <timofey.homich@gmail.com>
2023-12-14 14:46:04 +0200

    made a correct project structure

---
+++ TiVerse.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TiVerse.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
