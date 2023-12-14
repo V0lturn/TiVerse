1451f37b243ac64c8c39d371dacfe7f6c2e3c254
Tymofii Khomych <timofey.homich@gmail.com>
2023-12-14 15:37:46 +0200

    UserRouteHistory entity

---
+++ TiVerse.Core/Entity/UserRouteHistory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiVerse.Core.Entity
+{
+    internal class UserRouteHistory
+    {
+        public Guid UserID { get; set; }
+        public Guid TripID { get; set; }
+        public string Route { get; set; }
+        public string Transport { get; set; }
+    }
+}
