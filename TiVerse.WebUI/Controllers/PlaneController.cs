a3e2fe1867a9e6ac383837ff154cf464887a98dc
Tymofii Khomych <timofey.homich@gmail.com>
2023-12-18 21:42:30 +0200

    add bus, train, plane controllers to display 50 routes by each type of transopt. And use partial view to display them

---
+++ TiVerse.WebUI/Controllers/PlaneController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TiVerse.Core.Entity;
+using TiVerse.Infrastructure.AppDbContext;
+
+namespace TiVerse.WebUI.Controllers
+{
+    public class PlaneController : Controller
+    {
+        private readonly TiVerseDbContext _context;
+
+        public PlaneController(TiVerseDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Trip> busModel = new List<Trip>();
+            busModel = await _context.Trips.Where(t => t.Transport == "Plane").Take(50).ToListAsync();
+            return View("Index", busModel);
+        }
+    }
+}
---
+++ TiVerse.WebUI/Controllers/TrainController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TiVerse.Core.Entity;
+using TiVerse.Infrastructure.AppDbContext;
+
+namespace TiVerse.WebUI.Controllers
+{
+    public class TrainController : Controller
+    {
+        private readonly TiVerseDbContext _context;
+
+        public TrainController(TiVerseDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Trip> trainModel = new List<Trip>();
+            trainModel = await _context.Trips.Where(t => t.Transport == "Train").Take(50).ToListAsync();
+            return View("Index", trainModel);
+        }
+    }
+}
